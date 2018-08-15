aaa87438710cade21a6922f754612d849065a85c
LinnierGames <esericksanc@gmail.com>
2018-08-15 16:41:14 -0700

    cont.

---
+++ loaner/Loan.swift
@@ -0,0 +1,23 @@
+//
+//  Loan.swift
+//  loaner
+//
+//  Created by Erick Sanchez on 8/15/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+
+import Foundation
+import UIKit.UIImage
+
+struct Loan {
+    var itemTitle: String
+    var notes: String = ""
+    var itemImage: UIImage = UIImage(named: "no item image")!
+    var loanee: Loanee?
+}
+
+struct Loanee {
+    var name: String
+    var profileImage: UIImage = UIImage(named: "no profile image")!
+    var contactNumber: String?
+}
