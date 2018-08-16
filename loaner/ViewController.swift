80517b5d8a63ca1e3739e6af154ea406070a4218
LinnierGames <esericksanc@gmail.com>
2018-08-15 17:10:10 -0700

    layout collection view cell and flow layout

--- loaner/ViewController.swift
+++ loaner/ViewController.swift
@@ -26,6 +26,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //dummy data
         let pat = Loanee(name: "Pat", profileImage: #imageLiteral(resourceName: "pat"), contactNumber: "70712345678")
         let sponge = Loanee(name: "Spongebob", profileImage: #imageLiteral(resourceName: "sponge"), contactNumber: "7077654341")
         let krabs = Loanee(name: "Mr. Krabs", profileImage: #imageLiteral(resourceName: "mr krabs"), contactNumber: "7071357911")
@@ -35,10 +37,16 @@ class ViewController: UIViewController {
         let gameLoan = Loan(itemTitle: "Xbox Z", notes: "", itemImage: #imageLiteral(resourceName: "star"), loanee: krabs)
         
         items = [bottleLoan, phoneLoan, gameLoan]
-//        collectionView.reloadData()
+        
+        //set up collection view layout
+        let flow = UICollectionViewFlowLayout()
+        let screenSize = view.bounds.size
+        let horizontalPadding: CGFloat = 8
+        let verticalPadding: CGFloat = 12
+        flow.itemSize = CGSize(width: screenSize.width / 2 - horizontalPadding * 2, height: screenSize.width / 2 - verticalPadding * 2)
+        flow.sectionInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
+        collectionView.collectionViewLayout = flow
     }
-
-
 }
 
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
@@ -47,9 +55,16 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item cell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item cell", for: indexPath) as! ItemCollectionViewCell
+        
+        let item = items[indexPath.row]
+        cell.configure(item)
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath)
     }
 }
 
