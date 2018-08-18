b6ded3240ebeb0946067d1deb0495f003bc9a201
LinnierGames <esericksanc@gmail.com>
2018-08-18 13:16:47 -0700

    Populate with dummy values

--- loaner/ViewController.swift
+++ loaner/ViewController.swift
@@ -52,3 +52,16 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item cell", for: indexPath)
+        
+        return cell
+    }
+}
+
