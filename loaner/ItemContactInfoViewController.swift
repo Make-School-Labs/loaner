9bf518b73bfca305429c010cdfbabb5021cc1dbc
LinnierGames <esericksanc@gmail.com>
2018-08-17 22:44:28 -0700

    Update the UI from the given item

--- loaner/ItemContactInfoViewController.swift
+++ loaner/ItemContactInfoViewController.swift
@@ -11,5 +11,59 @@ import UIKit
 class ItemContactInfoViewController: UIViewController {
 
     var item: Item!
-
+    
+    func updateUI() {
+        labelTitle.text = "Contact Info"
+        imageViewItem.image = item.itemImage
+        labelItemTitle.text = item.itemTitle
+        if item.notes.isEmpty {
+            labelItemNotes.text = "No Notes"
+        } else {
+            labelItemNotes.text = item.notes
+        }
+        
+        updateContactInfo()
+    }
+    
+    func updateContactInfo() {
+        if let contact = item.loanee {
+            buttonAddContact.isHidden = true
+            stackViewContactInfo.isHidden = false
+            imageViewContact.image = contact.profileImage
+            labelContactName.text = contact.name
+            labelContactNumber.text = contact.contactNumber ?? "No Number"
+            
+        } else {
+            buttonAddContact.isHidden = false
+            stackViewContactInfo.isHidden = true
+        }
+    }
+    
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var imageViewItem: UIImageView!
+    @IBOutlet weak var labelItemTitle: UILabel!
+    @IBOutlet weak var labelItemNotes: UILabel!
+    @IBOutlet weak var buttonAddContact: UIButton!
+    @IBAction func pressAddContact(_ sender: UIButton) {
+        //TODO: present contact picker
+    }
+    
+    @IBOutlet weak var stackViewContactInfo: UIStackView!
+    @IBOutlet weak var imageViewContact: UIImageView!
+    @IBOutlet weak var labelContactName: UILabel!
+    @IBOutlet weak var labelContactNumber: UILabel!
+    @IBAction func pressDeleteContact(_ sender: UIButton) {
+        //TODO: clear loaner property and update ui
+    }
+    
+    @IBOutlet weak var buttonSave: UIButton!
+    @IBAction func pressSave(_ sender: UIButton) {
+        //TODO: validate input and unwind from save
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+    }
 }
